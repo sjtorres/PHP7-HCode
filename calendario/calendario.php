db1c44246a00f40fef34b0d5d830890c6be8aa0e
Sandro Torres <sandrotorres.ti@gmail.com>
2018-05-24 20:48:14 -0300

    Add pasta Calendário

---
+++ calendario/calendario.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @param $semana
+ */
+function linha($semana) {
+	echo "<tr>";
+
+	for ($i = 0; $i <= 6; $i++) {
+		if (isset($semana[$i])) {
+			echo "<td>{$semana[$i]}</td>";
+		} else {
+			echo "<td></td>";
+		}
+	}
+	echo "</tr>";
+}
+
+function calendario() {
+	$dia = 1;
+	$semana = [];
+
+	while ($dia <= 31) {
+		array_push($semana, $dia);
+
+		if (count($semana) == 7) {
+			linha($semana);
+			$semana = [];
+		}
+		$dia++;
+	}
+	linha($semana);
+}
+
+?>
+
+<table border="1">
+
+	<tr>
+		<th>Dom</th>
+		<th>Seg</th>
+		<th>Ter</th>
+		<th>Qua</th>
+		<th>Qui</th>
+		<th>Sex</th>
+		<th>Sab</th>
+	</tr>
+
+	<?php calendario();?>
+
+</table>
